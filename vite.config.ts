990512e95c11f64190cb76d6056504c4d4efb0d2
Егор Ревенков <revenkov.egor@gmail.com>
2023-09-07 09:57:27 +0300

    FIX CORS: конец

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/cian-api': {
-  //       target: 'https://cian.ru',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace("/cian-api", ''),
-  //     },
-  //   },
-  // },
+  server: {
+    port: 3005,
+  },
 })
