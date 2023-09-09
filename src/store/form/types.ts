3bd918b1ac2dce9c15de50c91064f3c61b3a780b
Tim <sadtim25@gmail.com>
2023-09-09 21:28:48 +0500

    Revert "Удалил sidebar.tsx"
    This reverts commit 1a25becce922d22b97f4cbd398568b5be764a41f.

--- src/store/form/types.ts
+++ src/store/form/types.ts
@@ -10,10 +10,16 @@ export type Form = {
     price?: FormParamRange
 }
 
-// Параметр с единственным значением. deal_type: {value: 2} => deal_type=2
-type FormParamSingle = {
-    value_type: "single", 
-    value: string
+// Параметры недвижимости
+
+type Realty = OfferTypeFlat | OfferTypeSuburban | OfferTypeOffices
+
+
+
+export type OfferTypeFlat = {
+    "offer_type"?: "flat"
+    "object_type[0]"?: Value1_2
+    "room"?: "room0" | "room1" | "room2" | "room3" | "room4" | "room5" | "room6" | "room7" | "room8" | "room9"
 }
 // Параметр с массивом значений. object_type: {value: [2,3]} => object_type=2&object_type=3
 type FormParamMulti = {
@@ -25,10 +31,31 @@ type FormParamMultiKey = {
     value_type: "multikey"
     value: string[]
 }
-type FormParamRange = {
-    value_type: "range", 
-    value: {
-        from?: number, 
-        to?: number,
-    }
+
+
+
+// Значения
+
+type Value1_2 = "1" | "2"
+type Value1_4 = "1" | "2" | "3" | "4"
+type Value1_12 = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12"
+type Value1_6 = "1" | "2" | "3" | "4" | "5" | "6" | "6300" | "6301" | "6302"
+
+
+// Неиспользуемые фильтры (пока)
+// Цена
+type Price = {
+    currency?: 2
+    minprice?: number
+    maxprice?: number
+}
+// Площадь
+type Area = {
+    minarea?: number
+    maxarea?: number
+}
+// Координаты (надо удалить)
+type Coordinates = {
+    lat?: number
+    lng?: number
 }
